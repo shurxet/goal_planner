b1f21c8a24bbcddb1d7fe81fa8e3e4a11dc162b0
shurxet <s.shurupov.88@mail.ru>
2023-04-18 08:51:22 +0300

    tg bot

--- backend/bot/tg/__init__.py
+++ backend/bot/tg/__init__.py
@@ -1,10 +1,12 @@
 from .data_models import Message, GetUpdatesResponse, SendMessageResponse
 from .client import TgClient
+from .fsm.memory_storage import MemoryStorage
 
 
 __all__ = [
     'Message',
     'GetUpdatesResponse',
     'SendMessageResponse',
-    'TgClient'
+    'TgClient',
+    'MemoryStorage'
 ]
